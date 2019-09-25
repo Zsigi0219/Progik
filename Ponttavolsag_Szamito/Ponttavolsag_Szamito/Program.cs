65d9cf2016134fde2b279727a61fb39a01068578
Séllei Zsigmond <31158052+Zsigi0219@users.noreply.github.com>
2019-09-25 17:46:27 +0200

    Add files via upload

---
+++ Ponttavolsag_Szamito/Ponttavolsag_Szamito/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponttavolsag_Szamito
+{
+    class Program
+    {
+        /*
+         * Írjunk programot, amely bekéri két pont koordinátáit,
+         * majd kiszámolja azok távolságát.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Első pont x koordinátája: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Első pont y koordinátája");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Második pont x koordinátája: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Második pont y koordinátája");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double tavolsag = Math.Sqrt((x1 - x2) * (x1 - x2) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Távolság: {0}", tavolsag);
+            Console.ReadLine();
+
+        }
+    }
+}
